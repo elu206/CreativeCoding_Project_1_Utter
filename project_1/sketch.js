b31d30c0434fcb2909cfd502ee4994fe557c29f8
elu206 <elu206@nyu.edu>
2020-10-20 19:54:25 -0700

    second push

--- project_1/sketch.js
+++ project_1/sketch.js
@@ -1,6 +1,9 @@
 flames = []
 smoke = []
 let counter = 0
+let noiseMax = 200
+let zoffset = 0;
+let size = 0;
 
 function setup() {
   createCanvas(600, 600);
@@ -8,25 +11,24 @@ function setup() {
 }
 
 function draw() {
-  background(50);
-  noStroke();
-  fill(255);
-  ellipse(width/2, height/2, 250)
-  fill(189, 174, 132);
-  rect(width/2 -15/2, height/2 - 50, 15, 220);
-  fill(232, 216, 172);
-  rect(width/2 - 15/2, height/2 - 50, 10, 220);
-  fill(148, 42, 38);
-  ellipse(width/2, height/2 - 60, 35, 45);
-  rect(width/2 - 15/2, height/2 - 45, 15, 15)
-  fill(186, 67, 63);
-  ellipse(width/2-3, height/2 - 60, 30, 45);
-  rect(width/2 - 15/2, height/2 - 45, 10, 15)
-  fill(237, 172, 166);
-  ellipse(width/2 - 19/2, height/2 - 70, 5, 10);
-  ellipse(width/2 - 23/2, height/2 - 60, 5, 5)
+  if (counter >= 0){
+    background(50);
+    text(counter, 20, 20); //to be deleted later, just for me to time things
+    noStroke();
+    myMatch();
+  }
 
-  text(counter, 20, 20); //to be deleted later, just for me to time things
+  if (counter >= 5){
+    push();
+    translate(width/2, 220);
+    if (size < 1){
+      size += 0.005
+    }
+    scale(size);
+    glow();
+    pop();
+    myMatch();
+  }
 
   if (counter >= 10){
     for (let i = 0; i < 10; i++) {
@@ -55,10 +57,57 @@ function draw() {
       }
     }
   }
-
 }
-
 
 function myCounter(){
   counter += 1;
+}
+
+function myMatch(){
+//match
+    fill(189, 174, 132);
+    rect(width/2 -15/2, height/2 - 50, 15, 220);
+    fill(232, 216, 172);
+    rect(width/2 - 15/2, height/2 - 50, 10, 220);
+    fill(148, 42, 38);
+    ellipse(width/2, height/2 - 60, 35, 45);
+    rect(width/2 - 15/2, height/2 - 45, 15, 15)
+    fill(186, 67, 63);
+    ellipse(width/2-3, height/2 - 60, 30, 45);
+    rect(width/2 - 15/2, height/2 - 45, 10, 15)
+    fill(237, 172, 166);
+    ellipse(width/2 - 19/2, height/2 - 70, 5, 10);
+    ellipse(width/2 - 23/2, height/2 - 60, 5, 5)
+
+}
+
+function glow(){
+  stroke(255, 0, 0);
+  strokeWeight(5);
+  fill(255, 187, 148);
+  beginShape();
+  for (let angle = 0; angle < TWO_PI; angle += 0.1){
+    let xoffset = map(cos(angle), -1, 1, 0, noiseMax);
+    let yoffset = map(sin(angle), -1, 1, 0, noiseMax);
+    let r = map(noise(xoffset, yoffset, zoffset), 0, 1, 50, 200);
+    let x = r * cos(angle);
+    let y = r * sin(angle);
+    vertex(x,y)
+  }
+  endShape(CLOSE);
+
+  stroke(255, 94, 0);
+  strokeWeight(3);
+  fill(255, 238, 140);
+  beginShape();
+  for (let angle = 0; angle < TWO_PI; angle += 0.1){
+    let xoffset = map(cos(angle), -1, 1, 0, noiseMax);
+    let yoffset = map(sin(angle), -1, 1, 0, noiseMax);
+    let r = map(noise(xoffset, yoffset, zoffset), 0, 1, 10, 200);
+    let x = r * cos(angle);
+    let y = r * sin(angle);
+    vertex(x,y)
+  }
+  endShape(CLOSE);
+  zoffset += 0.05
 }
--- project_1/smoke.js
+++ project_1/smoke.js
@@ -5,7 +5,6 @@ class Smoke{
     this.vx = random(-0.5, 0.5);
     this.vy = random(-20, -1);
     this.alpha = 255;
-    this.size = 10;
   }
   finished() {
     return this.alpha < 0;
@@ -14,14 +13,11 @@ class Smoke{
     this.x += this.vx;
     this.y += this.vy;
     this.alpha -= 10;
-    if (this.size > 0){
-      this.size -= 1
-    }
   }
   show() {
     noStroke();
     fill(100, this.alpha);
-    ellipse(this.x, this.y, this.size)
+    ellipse(this.x, this.y, 5)
   }
 
 }
